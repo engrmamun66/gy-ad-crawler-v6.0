90d75db5984fe665389a41bb1865be6ec233ee6d
engrmamun97 <engrmamun97@gmail.com>
2022-02-15 00:43:59 +0600

    Restore GUI.py

--- GUI.py
+++ GUI.py
@@ -1,320 +1,157 @@
-﻿from selenium import webdriver
-from selenium.webdriver.chrome.service import Service
-from selenium.webdriver.common.by import By
-from selenium.webdriver.support.ui import WebDriverWait
-from selenium.webdriver.support import expected_conditions as EC
-from bs4 import BeautifulSoup
+﻿import os
+import glob
+import xlsxwriter
+from tkinter import *
 from datetime import datetime
-import time
-import random
-import string
-import re
-import base64
-import os
+import crawler_soup
+global xlsxwriter
 
 
-# For Yourtube Text
-from inc.withimage import imgtotext
-from inc.functions import isYtAd, readYtAds, getYtTitle, getYtLink
-from inc.functions import isYsAd, getYsTitle, getYsPrice, getYsAnbieter
+class GUI():
+    def __init__(self):
+        #os.chdir(r''")
+        txt_files = myFiles = glob.glob('*.txt')
+        self.gui = Tk(className='google-youtube crawler')
+        # set window size
+        self.gui.geometry("750x350")
+        self.keywords_description = Label(
+            self.gui, text="Keywords bitte nur eins pro Zeile (mit Enter trennen)")
+        self.keywords_description.place(x=20, y=20)
+        self.keywords = Text(self.gui, bd=1, width=70, height=8)
+        self.keywords.place(x=20, y=40)
+        #self.time_start_description = Label(self.gui, text="Zu welcher Zeit möchtest du den Crawler starten? Format muss z.B (16:54) entsprechen")
+        #self.time_start_description.place(x=20, y=140)
+        #self.time_start = Entry(self.gui, bd=1, width=20)
+        #self.time_start.place(x=20, y=160)
+        self.options = txt_files
+        self.variable = StringVar(self.gui)
+        self.dropdownlist_init = self.variable.set(self.options[0])
+        self.dropdownlist = OptionMenu(self.gui, self.variable, *self.options)
+        self.dropdownlist.config(width=30)
+        self.dropdownlist.place(x=20, y=180)
+        self.start_crawler = Button(
+            self.gui, bd=1, width=80, height=5, text="Start", command=self.start)
+        self.start_crawler.place(x=20, y=240)
+        self.gui.mainloop()
 
+    def start(self):
+        kw_list = self.variable.get()
+        if self.keywords.compare("end-1c", "==", "1.0"):
+            keywords = open(kw_list, 'r', encoding='utf8')
+            results = {}
+            for keyword in keywords.readlines():
 
-#google_link_list = [] , google_title_list = [] , google_price_list = [] , google_anbieter_list = [] , youtube_link_list = [] , youtube_title_list = [] , youtube_price_list = [] , youtube_seller_list = [] ,
+                   try:
+                        results[keyword] = crawler_soup.read_ads(
+                            keyword, True)  # fff
+                        print(crawler_soup.read_ads(keyword, True))
+                    except Exception as e:
 
-def read_ads(input_keyword, open_browser=True):
-    screen_id = random.randint(1, 9999999999)
-    screen_id = random.choice(string.ascii_letters) + random.choice(
-        string.ascii_letters) + str(screen_id) + random.choice(string.ascii_letters)
-    stamp = str(datetime.now().strftime('%H_%M_%S %Y_%m_%d'))
-    # setup input and outputs
-    google_link_list = []
-    google_title_list = []
-    google_price_list = []
-    google_anbieter_list = []
-    google_ident_list = []
-    youtube_link_list = []
-    youtube_title_list = []
-    youtube_price_list = []
-    youtube_seller_list = []
-    youtube_ident_list = []
-    brand_list = []
-    rank_list = []
-    id_list = []
-    rank = 0
-
-    def getPriceFromTitle(title=''):
-        myTitle = title.strip().replace('    ', '   ').replace('   ', '  ').replace(
-            '  ', ' ').replace(' €', '€').replace('*, \d', ',\d')
-        result = ''
-        if('€' in myTitle):
-            for x in myTitle.split():
-                if('€' in x):
-                    result = x
-        return result.rstrip(" ,-“@!~$%^&*(){}[]\"'|\\/?<,>.`+*©zxcvbnm,.asdfghjklqwertyuiopZXCVBNM<ASDFGHJKL:QWERTYUIOP")
-
-    # open_browser = 1
-    if not open_browser:
-        options = webdriver.ChromeOptions()
-        options.add_argument('headless')
-        options.add_argument('--log-level=1')  # To stop warnings
-        ser = Service("chromedriver_win32/chromedriver.exe")
-        driver = webdriver.Chrome(service=ser, options=options)
-    else:
-        options = webdriver.ChromeOptions()
-        driver = webdriver.Chrome('chromedriver_win32/chromedriver.exe')
-
-    # =======================================
-    # =======================================
-    # ========== WITH GOOGLE ================
-    # =======================================
-    # =======================================
-
-    if 1:
-        driver.get(
-            "https://www.google.de/search?q={}".format(input_keyword.replace(" ", "+")))
-        content = driver.page_source.encode('utf-8').strip()
-        soup = BeautifulSoup(content, 'lxml')
-        try:
-            WebDriverWait(driver, 3).until(EC.element_to_be_clickable(
-                (By.XPATH, ("//*[text()='Ich stimme zu']")))).click()
-        except Exception as e:
-            WebDriverWait(driver, 3).until(EC.element_to_be_clickable(
-                (By.XPATH, "//*[@id='zV9nZe']"))).click()
-        # driver.switch_to.default_content()
-
-        # ==============================
-        # =========== Google Shopping Ad
-        # ==============================
-
-        # Controll Screen Shot
-        driver.set_window_size(1000, 1080)
-        time.sleep(1)
-        imageFileName = "C:\\Webcrawler\\Screens\\{}_g.png".format(screen_id)
-        driver.save_screenshot(imageFileName)
-        keepScreenShot = True
-
-        contents = soup.find_all('div', class_='mnr-c pla-unit')
-        rank = 0
-        for eachBlock in contents:
-
-            try:
-                link = eachBlock.find('div', class_='ropLT').find('a')['href']
-            except:
-                link = ''
-
-            try:
-                title = eachBlock.find('div', class_='r4awE').get_text()
-            except:
-                title = ''
-
-            # try: price = eachBlock.find('span', class_='e10twf').get_text()
-            try:
-                price = eachBlock.find(
-                    'span', text=re.compile(".*€.*")).get_text()
-            except:
-                price = '0.00€'
-
-            try:
-                anbieter = eachBlock.find(
-                    'div', class_='LbUacb').find('span').get_text()
-            except:
-                anbieter = ''
-
-            try:
-                ident_von = eachBlock.find('div', class_='Qp6aMc').get_text()
-            except:
-                ident_von = ''
-
-            if(
-                len(link) and link[0:4] == 'http'
-                and len(title)
-                and len(price)
-                and len(anbieter)
-                and len(ident_von)
-            ):
-                print(
-                    f"\n=============Google Shopping Ad===============\nlink : {link}\nTitle : {title}\nPrice : {price}\nAnbieter : {anbieter}\nident_von : {ident_von}\keyword: {input_keyword}")
-                keepScreenShot = False
-                rank += 1
-                rank_list.append(str(rank))
-                google_link_list.append(str(link))
-                google_title_list.append(str(title))
-                google_price_list.append(price)
-                google_anbieter_list.append(str(anbieter))
-                brand_list.append(str(ident_von))
-                google_ident_list.append("Google Shopping Ad")
-                id_list.append(str(screen_id) + "_gs")
-
-        # =================================
-        # =================================
-        # =========== Google Textanzeige Ad
-        # =================================
-        # =================================
-
-        contents = soup.find_all('div', class_='uEierd')
-        rank = 0
-        for eachBlock in contents:
-            try:
-                link = eachBlock.find('span', role="text").get_text()
-            except:
-                link = ''
-
-            try:
-                title = eachBlock.find('div', role='heading', class_='CCgQ5').find(
-                    'span').get_text()  # Note: may be change class_ name
-            except:
-                title = ''
-
-            try:
-                price = getPriceFromTitle(title)
-            except:
-                price = ''
-
-            try:
-                anbieter = eachBlock.find(
-                    'div', class_='v5yQqb').find('a')['href']
-            except:
-                anbieter = link
-
-            if(
-                len(link) and link[0:4] == 'http'
-                and len(title)
-                and len(anbieter)
-                and anbieter[0:4] == 'http'
-            ):
-                print(
-                    f"\n=============Google Textanzeige Ad===============\nlink : {link}\nTitle : {title}\nPrice : {price}\nAnbieter : {anbieter}\nkeyword: {input_keyword}")
-                keepScreenShot = False
-                rank += 1
-                rank_list.append(str(rank))
-                google_link_list.append(str(link))
-                google_title_list.append(str(title))
-                google_price_list.append(str(price))
-                google_anbieter_list.append(str(anbieter))
-                google_ident_list.append("Google Textanzeige")
-                id_list.append(str(screen_id) + "_gt")
-
-        if not keepScreenShot:  # found not fount any add delete the image
-            try:
-                os.remove(imageFileName)
-            except:
-                print("\nFailed Delete To Image!")
-
-    # =======================================
-    # =======================================
-    # ========== WITH YOUTUBE ===============
-    # =======================================
-    # =======================================
-
-    if 1:
-        # options = webdriver.ChromeOptions()
-        # driver = webdriver.Chrome('chromedriver_win32/chromedriver.exe')
-        driver.get(
-            "https://www.youtube.com/results?search_query={}".format(input_keyword.replace(" ", "+")))
-
-        try:
-            WebDriverWait(driver, 1.5).until(EC.element_to_be_clickable(
-                (By.XPATH, ("//*[text()='I Agree']")))).click()
-        except:
-            try:
-                WebDriverWait(driver, 1.5).until(EC.element_to_be_clickable(
-                    (By.XPATH, "//*[@id='yDmH0d']"))).click()
-            except:
-                pass
-
-        # driver.switch_to.default_content()
-        time.sleep(1)
-        imageFileName = "C:\\Webcrawler\\Screens\\{}_y.png".format(screen_id)
-        driver.save_screenshot(imageFileName)
-        keepScreenShot = True
-
-        # ===============================
-        # ===============================
-        # =========== Youtube Shopping Ad
-        # ===============================
-        # ===============================
-
-        hasShoppinAdd = isYsAd(imgtotext(
-            imagename=imageFileName, image_index=1, positionMap="200:900, 0:1500"))
-        if hasShoppinAdd:
-            gridMaps = ["300:860, 0:335",  # grid1
-                        "300:860, 335:620",  # grid2
-                        "300:860, 620:905",  # grid3
-                        "300:860, 905:1200",  # grid4
-                        ]
-            rank = 0
-            for gridMap in gridMaps:
-                imageText = imgtotext(imagename=imageFileName, image_index=1,
-                                      positionMap=gridMap, showimage=False, printText=False)
-
-                link = "https://www.youtube.com/results?search_query={}".format(
-                    input_keyword.replace(" ", "+"))
-                title = getYsTitle(imageText)
-                anbieter = getYsAnbieter(imageText)
-                price = getYsPrice(imageText)
-
-                if len(link) and len(title) and len(anbieter) and len(price):
-                    print(
-                        f'\n=============Youtube Shopping Ad===============\nTitle: {title}\nLink: {link}\nkeyword: {input_keyword}')
-                    rank += 1
-                    keepScreenShot = False
-                    rank_list.append(str(rank))
-                    google_link_list.append(str(link))
-                    google_title_list.append(str(title))
-                    google_price_list.append(price)
-                    google_anbieter_list.append(str(anbieter))
-                    google_ident_list.append("Youtube Shopping")
-                    id_list.append(str(screen_id) + "_ys")
-
-        # ==================================
-        # ==================================
-        # =========== Youtube Textanzeige Ad
-        # ==================================
-        # ==================================
-
-        imgMap_1 = "190:1500, 0:1500"
-        imageText = imgtotext(imagename=imageFileName, image_index=1,
-                              positionMap=imgMap_1, showimage=False, printText=False)
-
-        if isYtAd(imageText):
-            ads = readYtAds(imageText)
-            if len(ads):
-                rank = 0
-                for ad in ads:
-
-                    title = getYtTitle(ad)
-                    link = getYtLink(ad)
-                    anbieter = link
-
-                    if len(title) and len(link):
-                        print(
-                            f'\n=============Youtube Textanzeige Ad===============\nTitle: {title}\nLink: {link}\nkeyword: {input_keyword}')
-                        rank += 1
-                        keepScreenShot = False
-                        rank_list.append(str(rank))
-                        google_link_list.append(str(link))
-                        google_title_list.append(str(title))
-                        google_price_list.append('')
-                        google_anbieter_list.append(str(anbieter))
-                        google_ident_list.append("Youtube Textanzeige")
-                        id_list.append(str(screen_id) + "_yt")
-
+                        pass
+            self.safe(results)
         else:
-            print(f'\nThis is not an add !!!')
+            keywords = self.keywords.get("1.0", END).split("\n")
+            keywords = [keyword for keyword in keywords if keyword != ""]
+            results = {}
+            for keyword in keywords:
+                for i in range(1):
 
-        if not keepScreenShot:  # found not fount any add delete the image
-            os.remove(imageFileName)
+                    try:
+                        results[keyword] = self.crawl(keyword, False)
+                        if len(results[keyword][0]) > 0:
+                            break
+                        # print(results)
+                    except Exception as e:
 
-    # close web driver
-    driver.close()
-    # setup output
-    output = [google_link_list, google_title_list, google_price_list, google_anbieter_list, brand_list, google_ident_list, stamp,
-              id_list, rank_list, youtube_link_list, youtube_title_list, youtube_price_list, youtube_seller_list, youtube_ident_list]
-    #print(output)
-    print([len(google_link_list), len(google_title_list), len(google_price_list), len(google_anbieter_list), len(
-        brand_list), len(google_ident_list), len(youtube_price_list), len(youtube_seller_list), ])
-    return output
+                        pass
+            self.safe(results)
+
+
+    def crawl(self, keyword,driver):
+        return crawler_soup.read_ads(keyword, True)  # fff
+
+    def safe(self, results):
+        print('=============================')
+        print('========== THE END ==========')
+        # print('=============================')
+        # print('||||||||||||||||||||||| Stop to create excel')
+        # return
+        time_now = str(datetime.now().strftime('%Y_%m_%d_%H_%M_%S'))
+        workbook = xlsxwriter.Workbook('crawl_' + time_now + '.xlsx')
+        stopwords = open("stopwords.txt")
+        stopwords = stopwords.readlines()
+        #worksheet = workbook.add_worksheet("Anleitung")
+
+        time_now = str(datetime.now().strftime('%Y_%m_%d_%H_%M_%S'))
+        workbook = xlsxwriter.Workbook('crawl_' + time_now + '.xlsx')
+        stopwords = open("stopwords.txt")
+        stopwords = stopwords.readlines()
+        worksheet = workbook.add_worksheet("Crawlresults")
+        #worksheet = workbook.add_worksheet("Anleitung")
+        worksheet.write('A1', "Screen ID")  # google result
+        worksheet.write('B1', "Rank")  # google result
+        worksheet.write('C1', "Datum/Uhrzeit")  # google result
+        worksheet.write('D1', "Keyword")  # google result
+        worksheet.write('E1', "Link")  # google result
+        worksheet.write('F1', "Titel")  # google result
+        worksheet.write('G1', "Preis")  # youtube result
+        worksheet.write('H1', "Anbieter")  # youtube result
+        worksheet.write('I1', "Ad Anbieter")  # youtube result
+        worksheet.write('J1', "Ad Type")  # youtube result
+        zahl = 1
+        for i, (keyword, result) in enumerate(results.items()):
+            for j in range(0, len(result[0])):
+                   stopword = 0
+                    for word in stopwords:
+                        if word in result[0][j] or word in result[3][j]:
+                            stopword = 1
+                            zahl -= 1
+                            break
+                        else:
+                            continue
+                    zahl += 1
+                    if stopword == 0:
+                        try:
+                            # print(zahl)
+                            worksheet.write('A{}'.format(zahl), result[7][j])  # screen id
+                            worksheet.write('B{}'.format(zahl), result[8][j])  # rank
+                            worksheet.write('C{}'.format(zahl), result[6])  # datum uhrzeit
+                            worksheet.write('D{}'.format(zahl), keyword)  # datum uhrzeit
+                            worksheet.write('E{}'.format(zahl), result[0][j])  # google result
+                            worksheet.write('F{}'.format(zahl), result[1][j])  # google result
+                            worksheet.write('G{}'.format(zahl), result[2][j])  # google result
+                            worksheet.write('H{}'.format(zahl), result[3][j])  # google result
+                            worksheet.write('J{}'.format(zahl), result[5][j])  # google ident result
+                            worksheet.write('I{}'.format(zahl), result[4][j])  # Von Google / ..
+
+                        except:
+                            continue
+        workbook.close()
+        return
 
 
 if __name__ == "__main__":
-    read_ads("sommerreifen")
+    try:
+        # Directory
+        directory = "Webcrawler"
+        # Parent Directory path
+        parent_dir = "C:/"
+        # Path
+        path = os.path.join(parent_dir, directory)
+        os.mkdir(path)
+    except:
+        pass
+
+    try:
+        # Directory
+        directory = "Screens"
+        # Parent Directory path
+        parent_dir = "C:/Webcrawler"
+        # Path
+        path = os.path.join(parent_dir, directory)
+        os.mkdir(path)
+    except:
+        pass
+
+
+gui = GUI()
